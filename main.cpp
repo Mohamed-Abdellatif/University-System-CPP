8bb8290d2fe5bb1a2f57ccf45e2d428353df6bf7
Mohamed-Abdellatif <mabdellatif411@gmail.com>
2025-01-09 19:09:23 +0200

    initial main file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+// Additional includes can be added here as needed.
+
+using namespace std;
+
+// Function declarations (if any) can go here.
+
+int main() {
+    // Display a simple program header
+    cout << "===== C++ Program =====" << endl;
+
+    // TODO: Add your implementation here.
+
+    return 0;
+}
