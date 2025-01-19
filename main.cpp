79607c60a774bbd7d5806c8f3be1e7961b3e403b
Lark01 <omartamerkhaled@gmail.com>
2025-01-19 14:13:57 +0200

    readd comment for each person's part

--- main.cpp
+++ main.cpp
@@ -6,6 +6,28 @@
 
 using namespace std;
 
+//---------------------------------------ZEYAD Part----------------------------------
+// 1. Student Records Management: Use a Single Linked List (SLL) to store and manage
+// student details.
+// 2. Course Records Management: Implement a Binary Search Tree (BST) to organize
+// and search course information.
+//
+//---------------------------------------Omar Part----------------------------------
+// 3. Course Enrollment History: Employ a Double Linked List (DLL) to track each
+// student's course history.
+// 4. Course Prerequisites: Use a Stack to validate prerequisites for course registration.
+//
+//---------------------------------------Mazen Part---------------------------------
+// 5. Course Waitlists: Manage waitlists with a Queue.
+// 6. Search Optimization: Implement Linear and Binary Search algorithms.
+//
+// Important Notes
+// 1. Proper Variable Names
+// 2. Single responsibility principle
+// 3. Commit every time you add a new feature or change an old feature with a proper
+//    message that explains what happened
+// 4. Clean Code of course
+
 class PrerequisiteCourseStack;
 class Student;
 void startUniversityManagmentSystem();
